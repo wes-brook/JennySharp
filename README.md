5c238e9a53ac445052aa2563f4c1b45adab3280c
Wesly Barayuga <86134875+wes-brook@users.noreply.github.com>
2025-02-17 16:42:09 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Jenny Sharp | A booking service/portfolio for my hair stylist
+more TBA
+
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Jenny Sharp</h1>
+      {/* css container */}
+      {/* nav bar */}
+      {/* hero */}
+      {/* portfolio section */}
+      {/* services section */}
+      {/* credibility section */}
+      {/* footer */}
+    </div>
+  );
+}
